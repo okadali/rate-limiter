abd4faaf30d34cb0a5914d5d6639e852f9289a82
okadali <okadali@hotmail.com>
2026-07-08 22:34:32 +0300

    feat: reactiveredistemplate definition

--- src/main/java/com/okadali/rate_limiter/config/RedisConfig.java
+++ src/main/java/com/okadali/rate_limiter/config/RedisConfig.java
@@ -2,9 +2,12 @@ package com.okadali.rate_limiter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import tools.jackson.databind.ObjectMapper;
 
@@ -25,5 +28,21 @@ public class RedisConfig {
         template.afterPropertiesSet();
 
         return template;
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory, ObjectMapper objectMapper) {
+        final StringRedisSerializer keySerializer = new StringRedisSerializer();
+        final GenericJacksonJsonRedisSerializer genericJacksonJsonRedisSerializer = new GenericJacksonJsonRedisSerializer(objectMapper);
+
+        final RedisSerializationContext<String, Object> serializationContext =
+                RedisSerializationContext.<String, Object>newSerializationContext(keySerializer)
+                        .key(keySerializer)
+                        .value(genericJacksonJsonRedisSerializer)
+                        .hashKey(keySerializer)
+                        .hashValue(genericJacksonJsonRedisSerializer)
+                        .build();
+
+        return new ReactiveRedisTemplate<>(factory, serializationContext);
     }
 }
